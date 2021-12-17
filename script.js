b0db99bd6794cd2460fca6cd5e8a162f8a85c37b
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2021-12-17 15:17:11 -0300

    Requisito 7 e FIM DO PROJETO

--- script.js
+++ script.js
@@ -37,9 +37,39 @@ function btnWater(){
     container.style.border='5px double blue'
 }
 
-
 earth.addEventListener('click',btnEarth);
 function btnEarth(){
     container.style.border='none'
     container.style.border='6px groove green'
+}
+
+//Imagens Prontas
+let meme1 = document.getElementById('meme-1');
+let meme2 = document.getElementById('meme-2');
+let meme3 = document.getElementById('meme-3');
+let meme4 = document.getElementById('meme-4');
+
+meme1.addEventListener('click', exibeMeme1);
+meme2.addEventListener('click', exibeMeme2);
+meme3.addEventListener('click', exibeMeme3);
+meme4.addEventListener('click', exibeMeme4);
+
+
+function exibeMeme1(event){
+    document.getElementById('meme-image').src = event.target.src;
+}
+
+
+function exibeMeme2(event){
+    document.getElementById('meme-image').src = event.target.src;
+}
+
+
+function exibeMeme3(event){
+    document.getElementById('meme-image').src = event.target.src;
+}
+
+
+function exibeMeme4(event){
+    document.getElementById('meme-image').src = event.target.src;
 }
