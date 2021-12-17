4dbdcc602a249ddc745682f23e7fcab6a58f9196
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2021-12-17 14:13:12 -0300

    Requisito 06

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //Insere Legenda na Imagem
 const textoEntrada = document.getElementById('text-input');
 const textoSaida = document.getElementById('meme-text');
+const container = document.getElementById('meme-image-container');
 
 textoEntrada.addEventListener('keyup', exibeTexto);
 
@@ -17,3 +18,28 @@ function exibeImagem(event) {
         URL.revokeObjectURL(imagem.src) 
     }
 };
+
+
+//Botões
+const fire = document.getElementById('fire');
+const water = document.getElementById('water');
+const earth = document.getElementById('earth');
+
+fire.addEventListener('click',btnFire);
+function btnFire(){
+    container.style.border='none'
+    container.style.border='3px dashed red'
+}
+
+water.addEventListener('click',btnWater);
+function btnWater(){
+    container.style.border='none'
+    container.style.border='5px double blue'
+}
+
+
+earth.addEventListener('click',btnEarth);
+function btnEarth(){
+    container.style.border='none'
+    container.style.border='6px groove green'
+}
