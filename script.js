c0eb6f32f0e99733f0190b6485128045a974603a
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2021-12-17 02:00:46 -0300

    Requisito 02

--- script.js
+++ script.js
@@ -6,3 +6,29 @@ textoEntrada.addEventListener('keyup', exibeTexto);
 function exibeTexto(){
     textoSaida.innerHTML = textoEntrada.value;
 }
+
+
+//código da internet
+
+
+// let container = document.getElementById('meme-image-container');
+
+// function theimage(){
+//     const escolherArquivo = document.getElementById('meme-image').files[0].name;
+//     let imagem = document.createElement('img') ;
+//     imagem.innerHTML = '/' + escolherArquivo; 
+//     container.appendChild(imagem);
+ 
+//  console.log(escolherArquivo);
+// }
+
+
+let exibeImagem = function(event) {
+    let imagem = document.getElementById('meme-image');
+    imagem.src = URL.createObjectURL(event.target.files[0]);
+    imagem.onload = function() {
+        URL.revokeObjectURL(imagem.src) // free memory
+    }
+};
+
+
